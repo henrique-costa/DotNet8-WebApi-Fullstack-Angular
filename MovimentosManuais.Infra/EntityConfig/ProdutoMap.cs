cbf56a8c34d4671fdd91a7c93c5a08640b839b81
henrique-costa <henrique_vc@hotmail.com>
2024-08-27 17:11:43 -0300

    added repository pattern

--- MovimentosManuais.Infra/EntityConfig/ProdutoMap.cs
+++ MovimentosManuais.Infra/EntityConfig/ProdutoMap.cs
@@ -1,8 +1,18 @@
 using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovimentosManuais.ApplicationCore.Entity;
 
 namespace MovimentosManuais.Infra.EntityConfig;
 
-public class ProdutoMap
+public class ProdutoMap : IEntityTypeConfiguration<Produto>
 {
+    public void Configure(EntityTypeBuilder<Produto> builder)
+    {
+        builder.HasKey(x => x.COD_PRODUTO);
 
+        builder.Property(x => x.DES_PRODUTO).HasColumnType("VARCHAR(50)").IsRequired();
+
+        builder.Property(x => x.STA_STATUS).HasColumnType("CHAR(1)").IsRequired();
+    }
 }
--- MovimentosManuais.Infra/Repository/EFRepository.cs
+++ MovimentosManuais.Infra/Repository/EFRepository.cs
@@ -1,8 +1,46 @@
 using System;
+using System.Linq.Expressions;
+using MovimentosManuais.ApplicationCore.Interfaces.Repository;
+using MovimentosManuais.Infra.Data;
 
 namespace MovimentosManuais.Infra.Repository;
 
-public class EFRepository
+public class EFRepository<TEntity> : IRepository<TEntity> where TEntity : class
 {
+    protected readonly MovimentosManuaisContext _dbContext;
 
+    public EFRepository(MovimentosManuaisContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public TEntity Add(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TEntity> GetAll(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TEntity GetById(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TEntity> SearchBy(Expression<Func<TEntity, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
 }
